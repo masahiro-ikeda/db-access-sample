5c9c19fa034d89fc5551172c602eec0b737eafed
masahiro.ikeda <masahiro.ikeda@MyComputer>
2019-05-24 14:04:19 +0900

    DAO分割前のブランチを作成

--- src/main/java/work/MessageInsert.java
+++ src/main/java/work/MessageInsert.java
@@ -1,30 +1,86 @@
 package work;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 
+/*
+ * メッセージを投稿してデータベースに保存する。
+ * コマンドライン引数で次の値を引き渡す。
+ *  - 投稿ID
+ *  - 投稿者名
+ *  - 投稿内容
+ *
+ *  例：1 "ikeda" "Hello World!!"
+ */
 public class MessageInsert {
 
-	/*
-	 * メッセージを投稿してデータベースに保存する。
-	 * コマンドライン引数で次の値を引き渡す。
-	 *  - 投稿ID
-	 *  - 投稿者名
-	 *  - 投稿内容
-	 *
-	 *  例：1 "ikeda" "Hello World!!"
-	 */
+	// データベース接続に必要なデータ
+	private static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
+	private static final String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:ORCL";
+	private static final String USER_ID = "imuser";
+	private static final String USER_PASS = "impass";
+
 	public static void main(String[] args) {
 
-		// コマンドラインで入力した値をDtoクラスに格納
-		MessageDto dto = new MessageDto();
-		dto.setId(Integer.parseInt(args[0]));
-		dto.setName(args[1]);
-		dto.setMessage(args[2]);
-		dto.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+		if (args.length != 3) {
+			System.out.println("コマンドライン引数の数が不適切です。");
+			System.exit(0);
+		}
 
-		// Daoクラスの呼び出し
-		MessageDao dao = new MessageDao();
-		dao.insert(dto);
+		int id = Integer.parseInt(args[0]);
+		String name = args[1];
+		String message = args[2];
+
+		try {
+			Class.forName(DRIVER_NAME);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			con = DriverManager.getConnection(JDBC_URL, USER_ID, USER_PASS);
+
+			StringBuilder builder = new StringBuilder();
+			builder.append("INSERT INTO message_board ( ");
+			builder.append("    id,                     ");
+			builder.append("    name,                   ");
+			builder.append("    message,                ");
+			builder.append("    created_at              ");
+			builder.append(") VALUES (                  ");
+			builder.append("    ?,                      ");
+			builder.append("    ?,                      ");
+			builder.append("    ?,                      ");
+			builder.append("    ?                       ");
+			builder.append(")                           ");
+
+			ps = con.prepareStatement(builder.toString());
+			ps.setInt(1, id);
+			ps.setString(2, name);
+			ps.setString(3, message);
+			ps.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
+
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 
 		System.out.println("メッセージを投稿しました。");
 	}
--- src/main/java/work/MessageSelect.java
+++ src/main/java/work/MessageSelect.java
@@ -1,27 +1,77 @@
 package work;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+/**
+ * データベースから投稿メッセージを取得して出力する
+ */
 public class MessageSelect {
 
-	/*
-	 * データベースから投稿メッセージを取得して出力する
-	 */
+	// データベース接続に必要なデータ
+	private static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
+	private static final String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:ORCL";
+	private static final String USER_ID = "imuser";
+	private static final String USER_PASS = "impass";
+
 	public static void main(String[] args) {
 
-		// Daoクラスの呼び出し
-		MessageDao dao = new MessageDao();
-		ArrayList<MessageDto> list = dao.select();
+		try {
+			Class.forName(DRIVER_NAME);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
-		// 拡張for文で1行ずつ取り出して出力
-		for (MessageDto dto : list) {
-			System.out.println("=====================================");
-			System.out.println("id         : " + dto.getId());
-			System.out.println("name       : " + dto.getName());
-			System.out.println("message    : " + dto.getMessage());
-			System.out.println("created_at : " + dto.getCreatedAt());
-			System.out.println("=====================================");
-			System.out.println();
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DriverManager.getConnection(JDBC_URL, USER_ID, USER_PASS);
+
+			StringBuilder builder = new StringBuilder();
+			builder.append("SELECT          ");
+			builder.append("  id,           ");
+			builder.append("  name,         ");
+			builder.append("  message,      ");
+			builder.append("  created_at    ");
+			builder.append("FROM            ");
+			builder.append("  message_board ");
+			builder.append("ORDER BY        ");
+			builder.append("  id            ");
+
+			ps = con.prepareStatement(builder.toString());
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				System.out.println("=====================================");
+				System.out.println("id         : " + rs.getInt("id"));
+				System.out.println("name       : " + rs.getString("name"));
+				System.out.println("message    : " + rs.getString("message"));
+				System.out.println("created_at : " + rs.getTimestamp("created_at"));
+				System.out.println("=====================================");
+				System.out.println();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (ps != null) {
+					ps.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
